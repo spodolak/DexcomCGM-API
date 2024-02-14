8c8827bc5293c2c0a5860db8b13a6b9540c284cc
Podolak <macbook@Podolaks-MacBook-Pro.local>
2024-02-13 17:24:25 -0800

    update port

--- server.js
+++ server.js
@@ -76,4 +76,4 @@ let getBloodSugars = (token) => {
 }
 
 
-app.listen(8000, () => {console.log("Server started on port 8000")});
+app.listen(process.env.PORT || 8000, () => {console.log("Server started on port 8000")});
